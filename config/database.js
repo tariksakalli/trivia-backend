ff167f16345e84b77235b596369236ef41df73e4
Tarık Sakallı <tarik.sakalli@outlook.com>
2020-06-16 02:29:15 +0300

    Configure database connection

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const { createPool } = require('mysql');
+
+const pool = createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  connectionLimit: 100,
+});
+
+module.exports = pool;
