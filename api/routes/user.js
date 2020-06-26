3eb5ae586a768779bcbc4db07a43fdc03ebfa5b5
Tarık Sakallı <tarik.sakalli@outlook.com>
2020-06-26 15:33:45 +0300

    Update user.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -20,12 +20,12 @@ router.post('/survey', (req, res) => {
 });
 
 router.post('/test-result', verifyToken, (req, res) => {
-  const sql = `INSERT INTO tests (studentname, testname, date, totaltime, answers)
-    VALUES (?, ?, ?, ?, JSON_ARRAY(?))`;
+  const sql = `INSERT INTO tests (studentname, testname, testtype, date, totaltime, answers)
+    VALUES (?, ?, ?, ?, ?, ?)`;
   const params = { ...req.body };
   const answersJSON = JSON.stringify(params.answers);
   pool.query(sql,
-    [params.username, params.testName, params.date, params.totalTime, answersJSON],
+    [params.username, params.testName, params.testType, params.date, params.totalTime, answersJSON],
     (err, result) => {
       if (err) {
         res.status(404).json({ message: err });
